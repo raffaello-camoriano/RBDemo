8d58b3104442efdbdbe236335620b6e43e96c524
Ommac <raffaello.camoriano@iit.it>
2014-05-05 21:13:23 +0200

    Removed getArmDependentOptions method

--- modules/openCloseHandModule/src/main.cpp
+++ modules/openCloseHandModule/src/main.cpp
@@ -182,6 +182,16 @@ public:
         char * receivedCmd = new char [command.get(0).asString().length()+1];
         strcpy (receivedCmd, command.get(0).asString().c_str());
 
+        string receivedCmd = command.get(0).asString().c_str();
+        
+        //OR
+        
+        string receivedCmd = command.get(0).asString();
+        
+        //???
+        
+        
+        
         // Stop current motion and clear actions queue
         actionL->stopControl();
         actionR->stopControl();
@@ -192,7 +202,7 @@ public:
 
         reply.clear();
 
-        if (strcmp(receivedCmd , "open_left_hand") == 0)
+        if receivedCmd == "open_left_hand"
         {
 
             actionL->pushAction("open_hand");
@@ -207,7 +217,7 @@ public:
                 //cout<<"Left hand fully opened"<<endl;
         }
 
-        else if (strcmp(receivedCmd , "open_right_hand") == 0)
+        else if receivedCmd == "open_right_hand"
         {
             actionR->pushAction("open_hand");
             actionR->checkActionsDone(f,true);
@@ -221,7 +231,7 @@ public:
                 //cout<<"Right hand fully opened"<<endl;
         }
 
-        else if (strcmp(receivedCmd , "close_right_hand") == 0)
+        else if receivedCmd == "close_right_hand"
         {
             actionR->pushAction("close_hand");
             actionR->checkActionsDone(f,true);
@@ -235,7 +245,7 @@ public:
                 //cout<<"Right hand fully closed"<<endl;
         }
 
-        else if (strcmp(receivedCmd , "close_left_hand") == 0)
+        else if receivedCmd == "close_left_hand"
         {
             actionL->pushAction("close_hand");
             actionL->checkActionsDone(f,true);
@@ -253,62 +263,6 @@ public:
         return true;
     }
 
-    /************************************************************************/
-    void getArmDependentOptions(Bottle &b, Vector &_gOrien, Vector &_gDisp,
-                                Vector &_dOffs, Vector &_dLift, Vector &_home_x)
-    {
-        if (Bottle *pB=b.find("grasp_orientation").asList())
-        {
-            int sz=pB->size();
-            int len=_gOrien.length();
-            int l=len<sz?len:sz;
-
-            for (int i=0; i<l; i++)
-                _gOrien[i]=pB->get(i).asDouble();
-        }
-
-        if (Bottle *pB=b.find("grasp_displacement").asList())
-        {
-            int sz=pB->size();
-            int len=_gDisp.length();
-            int l=len<sz?len:sz;
-
-            for (int i=0; i<l; i++)
-                _gDisp[i]=pB->get(i).asDouble();
-        }
-
-        if (Bottle *pB=b.find("systematic_error_displacement").asList())
-        {
-            int sz=pB->size();
-            int len=_dOffs.length();
-            int l=len<sz?len:sz;
-
-            for (int i=0; i<l; i++)
-                _dOffs[i]=pB->get(i).asDouble();
-        }
-
-        if (Bottle *pB=b.find("lifting_displacement").asList())
-        {
-            int sz=pB->size();
-            int len=_dLift.length();
-            int l=len<sz?len:sz;
-
-            for (int i=0; i<l; i++)
-                _dLift[i]=pB->get(i).asDouble();
-        }
-
-        if (Bottle *pB=b.find("home_position").asList())
-        {
-            int sz=pB->size();
-            int len=_home_x.length();
-            int l=len<sz?len:sz;
-
-            for (int i=0; i<l; i++)
-                _home_x[i]=pB->get(i).asDouble();
-        }
-    }
-
-    /************************************************************************/
     bool configure(ResourceFinder &rf)
     {
         string name=rf.find("name").asString().c_str();
