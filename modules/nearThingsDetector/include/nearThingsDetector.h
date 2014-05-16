01ca58fa83129793741c702ecab3796f99de0862
Tanis Mar <tanis.mar@iit.it>
2014-05-16 17:12:31 +0200

    [NTD] Updated module, rpc and added verbose Updated RBdemoapp with available modules

--- modules/nearThingsDetector/include/nearThingsDetector.h
+++ modules/nearThingsDetector/include/nearThingsDetector.h
@@ -66,6 +66,7 @@ private:
     int backgroundThresh;
     int cannyThresh;
     int minBlobSize;
+    bool verbose;
     
     int dispThreshRatioLow;
     int dispThreshRatioHigh;
@@ -87,6 +88,7 @@ public:
     bool setOrigin(std::vector<double> origin);
     bool setRange(double range);
     bool setThresh(int thresh);
+    bool setVerbose(std::string verb);
     
     bool        open();
     void        close();
