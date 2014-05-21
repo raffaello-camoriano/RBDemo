b43e8f88b87a6954f68f59148959a6de0914b802
Ommac <raffaello.camoriano@iit.it>
2014-05-21 18:25:20 +0200

    modified port monitor

--- app/conf/handCtrlMonitor.lua
+++ app/conf/handCtrlMonitor.lua
@@ -3,6 +3,24 @@
 -- Author: Raffaello Camoriano
 -- CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 --
+
+-- How to use the portMonitor
+
+-- In general:
+--  $ yarp connect /out /in tcp+recv.portmonitor+script.lua+context.myapp+file.my_lua_script
+  
+--  'script.lua' tells the carrier to create a port monitor object for Lua.  
+--  'context.myapp' tells the resource finder to load the script from the 'myapp' context. 
+-- 'file.my_lua_script' indicates 'my_lua_script' should be loaded by monitor object. 
+--  'my_lua_script' is located using standard yarp Resource Finder policy. The postfix 
+--  (e.g., '.lua') is not necessary.
+
+-- In our case:
+-- yarp connect /reachModule/handToBeClosed:o /handCtrl/handToBeClosed:i tcp+recv.portmonitor+script.lua+context.RBDemo+file.handCtrlMonitor
+ 
+-- yarp connect /touchDetector/contact_pos:o /handCtrl/handToBeClosed:i tcp+recv.portmonitor+script.lua+context.RBDemo+file.handCtrlMonitor
+
+
  
 -- loading lua-yarp binding library
 require("yarp")
